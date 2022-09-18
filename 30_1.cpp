e935d6a07db4e2b0ff8d611da861246052349816
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-09-19 05:55:40 +0600

    First Commit and Created a treeNode class

---
+++ 30_1.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class treeNode
+{
+public:
+    int data;
+    treeNode *leftChild, *rightChild;
+
+    treeNode(int val)
+    {
+        this->data = val;
+        this->leftChild = this->rightChild = NULL;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
