f0e76b506fe0fd835691aec6faac5f6e44350bdc
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-09-19 06:01:28 +0600

    Ready for Tree Input

--- 30_1.cpp
+++ 30_1.cpp
@@ -16,6 +16,29 @@ public:
 
 int main()
 {
+    int n;
+    cin >> n;
+    treeNode *allNodes[n];
+    for (int i = 0; i < n; i++)
+    {
+        allNodes[i]->data = -1;
+    }
 
+    for (int i = 0; i < n; i++)
+    {
+        int value, left, right;
+        cin >> value >> left >> right;
+        allNodes[i]->data = value;
+
+        if (left != -1)
+        {
+            allNodes[i]->leftChild = allNodes[left];
+        }
+
+        if (right != -1)
+        {
+            allNodes[i]->rightChild = allNodes[right];
+        }
+    }
     return 0;
 }
